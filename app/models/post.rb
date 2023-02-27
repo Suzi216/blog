c3307931ed1b10b19dc864d4a3b94d90d1895000
Suzi216 <suzana.marsela@gmail.com>
2023-02-27 15:33:05 +0100

    Fixing some errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,8 +13,6 @@ class Post < ApplicationRecord
 
   after_save :update_posts_counter
 
-  private
-
   def update_posts_counter
     author.increment!(:posts_counter)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_24_172358) do
 
   create_table "comments", force: :cascade do |t|
     t.text "text"
-    t.bigint "author_id"
-    t.bigint "post_id"
+    t.bigint "author_id", null: false
+    t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_comments_on_author_id"
@@ -25,8 +25,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_24_172358) do
   end
 
   create_table "likes", force: :cascade do |t|
-    t.bigint "author_id" 
-    t.bigint "post_id"
+    t.bigint "author_id", null: false
+    t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_likes_on_author_id"
@@ -36,11 +36,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_24_172358) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "text"
-    t.bigint "author_id"
+    t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "comments_counter"
-    t.integer "likes_counter"
+    t.integer "comments_counter", default: 0
+    t.integer "likes_counter", default: 0
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_24_172358) do
     t.string "name"
     t.string "photo"
     t.text "bio"
-    t.integer "posts_counter"
+    t.integer "posts_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
