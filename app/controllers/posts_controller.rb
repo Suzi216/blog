7df3d0ff31c2d60388ca29770adf49340b723e0c
Suzi216 <suzana.marsela@gmail.com>
2023-02-28 22:37:29 +0100

    Adding the initial views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,11 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find(params[:author_id])
+  end
 
-  def show; end
+  def show
+    @post = Post.find(params[:id])
+    @user = User.find(params[:author_id])
+    @comments = @post.comments
+  end
 end
