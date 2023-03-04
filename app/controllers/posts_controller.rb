8e5c79bf329767bc732894c646239e6febc5dc26
Suzi216 <suzana.marsela@gmail.com>
2023-03-04 20:03:53 +0100

    Adding the forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,31 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find(params[:author_id])
+    @user= current_user
+    @posts = Post.all.where(author_id: @user.id)
+    # @user = User.find(params[:author_id])
   end
 
   def show
     @post = Post.find(params[:id])
-    @user = User.find(params[:author_id])
-    @comments = @post.comments
+    @user = current_user
+    @comment= Comment
+    # @comments/ = @post.comments
   end
+
+  def create
+    @post=Post.new(params.require(:post).permit(:title , :text))
+    @user = current_user
+    @post.author_id = @user.id
+    if @post.save
+      redirect_to user_posts_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def new
+      @post =Post.new
+  end
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
    root "users#index"
-    # resources :users, only: [:index, :show] do
-    #   resources :posts, only: [:index, :show]
-    # end
+    resources :users, only: [:index, :show] do
+      resources :posts, only: [:index, :show, :new, :create] do
+        resources :comments, only: [:new, :create]
+        resources :likes, only:[:create]
+      end
+    end
     
-   get "users/:id", to: "users#show"
-   get "users/:author_id/posts", to: "posts#index"
-   get "users/:author_id/posts/:id", to: "posts#show"
+  #  get "users/:id", to: "users#show"
+  #  get "users/:author_id/posts", to: "posts#index"
+  #  get "users/:author_id/posts/:id", to: "posts#show"
+
 end
